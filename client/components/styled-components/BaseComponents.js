2faa597228505a5d1f8cfc6c7bb5de1b891d6014
James O'Reilly <James.Oreilly926@gmail.com>
2019-06-12 14:14:00 -0400

    update Flex Container styled components to be centered

--- client/components/styled-components/BaseComponents.js
+++ client/components/styled-components/BaseComponents.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const FlexRowContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center
 `
 
 export const FlexColumnContainer = styled(FlexRowContainer)`
