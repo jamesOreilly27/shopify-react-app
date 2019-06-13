9cdacb468ad92edbbcdb8a1226cd370e859f0b09
James O'Reilly <James.Oreilly926@gmail.com>
2019-06-13 14:54:19 -0400

    create a backend route for '/api/customers/all' that converts data for all customers to json

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ const dotenv = require('dotenv')
 const express = require('express')
 const router = express.Router()
 const products = require('./products')
+const customers = require('./customers')
 
 // const baseUrl = `https://${process.env.SHOPIFY_API_KEY}:${process.env.PASSWORD}@dbny-code-test.myshopify.com/admin/api/2019-04`
 
 router.use('/products', products)
+router.use('/customers', customers)
 
 module.exports = router
