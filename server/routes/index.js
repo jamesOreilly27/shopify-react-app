3c475d394d0337951953cd9d471a54a34c1e0437
James O'Reilly <James.Oreilly926@gmail.com>
2019-06-13 14:54:36 -0400

    remove old comment in code

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,6 @@ const router = express.Router()
 const products = require('./products')
 const customers = require('./customers')
 
-// const baseUrl = `https://${process.env.SHOPIFY_API_KEY}:${process.env.PASSWORD}@dbny-code-test.myshopify.com/admin/api/2019-04`
-
 router.use('/products', products)
 router.use('/customers', customers)
 
