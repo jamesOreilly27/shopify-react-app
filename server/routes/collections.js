0f10177557631868da63e476bb32688b6b0fd479
James O'Reilly <James.Oreilly926@gmail.com>
2019-06-14 12:59:38 -0400

    make a backend route at '/api/collections/custom/all' that stores json data for all custom collections

---
+++ server/routes/collections.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const axios = require('axios')
+const baseUrl = require('./baseUrl')
+
+router.get('/custom/all', (req, res, next) => {
+  axios.get(`${baseUrl}/custom_collections.json`)
+  .then(res => res.data)
+  .then(collections => res.json(collections))
+  .catch(console.error)
+})
+
+module.exports = router
