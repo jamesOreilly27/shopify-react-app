17b902bf3a8140d1f134508ab80e07dc795e2433
James O'Reilly <James.Oreilly926@gmail.com>
2019-06-14 12:11:08 -0400

    create a backend route for storing all reports with data from shopify

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router()
 const products = require('./products')
 const customers = require('./customers')
 const orders = require('./orders')
+const reports = require('./reports')
 
 router.use('/products', products)
 router.use('/customers', customers)
 router.use('/orders', orders)
+router.use('/reports', reports)
+
 
 module.exports = router
