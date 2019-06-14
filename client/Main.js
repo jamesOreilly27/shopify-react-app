05d91a2d609143f20247132f9d21f9bd07dc2dad
James O'Reilly <James.Oreilly926@gmail.com>
2019-06-14 10:36:43 -0400

    funnel all Component files through client/components/index.js for ease importing

--- client/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { FlexRowContainer, FlexColumnContainer } from './components/styled-components/BaseComponents'
-import CustomerList from './components/CustomerList'
-
-const Main = () => (
-  <CustomerList />
-)
-
-export default Main
---
+++ client/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FlexRowContainer, FlexColumnContainer } from './styled-components/BaseComponents'
+import { CustomerList } from '../components'
+
+const Main = () => (
+  <CustomerList />
+)
+
+export default Main
